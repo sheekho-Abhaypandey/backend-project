e0f89570a049884076f72785788c8f5d81dc381f
Abhay Pandey <abhaypandeyaman@gmail.com>
2025-03-20 23:59:07 +0530

    fix(app): replace cookies-parser with cookie-parser and integrate user routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import cookies-parser from 'cookies-parser';
+import cookieParser from 'cookie-parser';
 const app=express();
+
 app.use(cors({origin:process.env.CORS_ORIGIN,
 Credentials:true
 }));
@@ -9,6 +10,15 @@ Credentials:true
 app.use(express.json({ limit:"16kb"}));
 app.use(express.urlencoded({extended:true , limit:"16kb"}));
 app.use(express.static("public"));
-app.use(cookies-parser());
+app.use(cookieParser());
+
+
+
+//import router below from these lines
+import userRouter from './routes/user.routes.js';
+
+  
+
+app.use("/api/v1/user",userRouter);
 
 export  {app};
